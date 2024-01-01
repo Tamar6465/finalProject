a83616578ad7024e9a325640d29d71d55e5e5fd3
tamar6465 <tamarf6465@gmail.com>
2024-01-01 13:56:43 +0200

    commit change 1.1

--- routers/resort.router.js
+++ routers/resort.router.js
@@ -3,12 +3,12 @@ const { getAllResort,getResortByCity,getResortByDisabled,addResort,updateResort,
 const { auth } = require("../middlewares/auth");
 const router = express.Router();
 
-router.get("/getAll", getAllResort);
+router.get("/getAll",auth(), getAllResort);
 router.get("/getByCity/:city",auth(), getResortByCity);
 router.get("/getByDisabled/:disable",auth(), getResortByDisabled);
 router.get("/getResortByPrice",auth(),getbyPrice)
 router.post("/addResort", auth(),addResort);
-router.put("/updateResort/:id",updateResort);
-router.delete("/deleteResort/:id",deleteResort)
+router.put("/updateResort/:id",auth(),updateResort);
+router.delete("/deleteResort/:id",auth(),deleteResort)
 
 module.exports = router;
