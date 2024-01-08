8d68bad473af8a315b2c6a9401899ee5308fad5e
ayala-shtreicher <a0504169337@gmail.com>
2024-01-08 11:15:02 +0200

    08.01.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ export default function NavBar() {
     //  }  
   }, [])
   return (
-    <div className='bg-info-subtle'>
+    <div className='bg-secondary-subtle'>
       <nav className='mb-5'>
         <ul className='d-flex list-unstyled align-items-center'>
           <li className='m-2'>
